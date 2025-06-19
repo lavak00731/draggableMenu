ce7a872da4d45c0592fda97088fa92cdf69edf61
Ezequiel <eolavaca@gmail.com>
2025-06-19 20:31:52 -0300

    changing icon assingation function to add special icons to info and ending

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,12 +3,12 @@ import type { pagesTypes } from "../types/pagesTypes"
 import { Plus, Info, CircleCheck, FileText, EllipsisVertical } from 'lucide-react';
 
 export const Nav = ({pages}:{ pages: pagesTypes[] }) => {
-  const iconsAssignation = (i: number) => {
-    switch (i) {
-      case 0:
+  const iconsAssignation = (name: string) => {
+    switch (name) {
+      case "info":
         return <Info  size={20}/>
 
-      case pages.length - 1:
+      case "ending":
         return <CircleCheck size={20}/>
 
       default:
@@ -20,10 +20,10 @@ export const Nav = ({pages}:{ pages: pagesTypes[] }) => {
     <nav className="bg-gray-50 drop-shadow-md p-5 border-neutral-200 border-t">
         <ul className="flex flex-row">
             {
-                pages.map((page:pagesTypes, i:number) =>{
+                pages.map((page:pagesTypes) =>{
                     return <li className="py-1.5 px-2.5 border-neutral-200 rounded-lg bg-gray-400/[.15] text-slate-500 hover:bg-gray-400/[.35] focus-within:border-blue-600 focus-within:border-1 focus-within:bg-white " key={page.id}>
                                <NavLink className="flex flex-row items-center gap-1.5 font-inter leading-5 capitalize font-medium text-sm  focus-visible:text-zinc-900 focus-visible:outline-0" to={page.route}>
-                                  {iconsAssignation(i)}
+                                  {iconsAssignation(page.name)}
                                   {page.name}
                                   <EllipsisVertical  />
                                 </NavLink>
