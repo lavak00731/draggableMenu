77ac03aad92f249e4a55c3bf65dab09e566565a9
Ezequiel <eolavaca@gmail.com>
2025-06-19 09:11:23 -0300

    styling elements

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,21 @@
 import { NavLink } from "react-router-dom"
 import type { pagesTypes } from "../types/pagesTypes"
+import { Plus } from 'lucide-react';
+
 export const Nav = ({pages}:{ pages: pagesTypes[] }) => {
   return (
     <nav className="bg-gray-50 drop-shadow-md p-5 border-neutral-200 border-t">
-        <ul>
+        <ul className="flex flex-row gap-x-5">
             {
                 pages.map((page:pagesTypes) =>{
-                    return <li key={page.id}>
-                                <NavLink to={page.route}>{page.name}</NavLink>                        
+                    return <li className="py-1.5 px-2.5 border rounded-lg" key={page.id}>
+                               <NavLink className="flex flex-row items-center gap-1.5 font-inter leading-5  capitalize font-medium text-sm" to={page.route}>
+                                  {page.name}
+                                </NavLink>                        
                             </li>
                 })
             }
+            <li className="py-1.5 px-2.5 border rounded-lg"><button className="flex flex-row items-center gap-1.5 font-inter leading-5 capitalize font-medium text-sm"><Plus size={16}/> Add Pages</button></li>
         </ul>
     </nav>
   )
--- src/views/Page.tsx
+++ src/views/Page.tsx
@@ -2,8 +2,8 @@ import type { PageViewProps } from "../types/pageViewType";
 
 export const Page = ({ content }: PageViewProps) => {
   return (
-    <article className="bg">
-        <h1>Hello {content}</h1>
+    <article className="bg-blue-700 flex items-center justify-center h-[80lvh]">
+        <h1 className="text-white text-5xl capitalize">Hello {content}</h1>
     </article>
   );
 };
